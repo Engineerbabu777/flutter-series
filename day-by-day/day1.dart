c088de9a0ebb34fd2dec966785511cdaed3d4ef9
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-01 19:16:55 +0500

    Added-day-1

---
+++ day-by-day/day1.dart
@@ -0,0 +1,14 @@
+
+
+import "package:flutter/material.dart";
+
+void main(){
+
+  runApp(
+    const Center(
+      child:Text('Hello To Flutter',
+      textDirection: TextDirection.ltr,
+    ),
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+import "package:flutter/material.dart";
+
+
+
+void main(){
+
+  runApp(
+    const Center(
+      child:Text('Hello To Flutter',
+      textDirection: TextDirection.ltr,
+    ),
+    ),
+  );
+}
